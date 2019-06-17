618bd531ab416504e4b0a8260b6bef41f5190dfb
devwithu <devwithu@naver.com>
2019-06-17 09:39:25 +0900

    first commit

---
+++ Assets/Sample01.cs
@@ -0,0 +1,13 @@
+﻿     using UnityEngine; 
+     using System.Collections;
+     using System.Runtime.InteropServices;
+
+public class Sample01 : MonoBehaviour {
+
+         [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+         public static extern void OutputDebugString(string message);
+         void Start() 
+         {
+             OutputDebugString("started...");
+         }
+}
